e871eb63856d0f7ad30f4ef0f66fb8616da32bdb
arthurjulio <arthurjuliorisa@gmail.com>
2020-05-09 08:51:20 +0700

    add font google to local css

--- application/views/backend/header.php
+++ application/views/backend/header.php
@@ -10,7 +10,8 @@
     <!-- Favicon -->
     <link rel="icon" href="<?= base_url('assets_backend') ?>/img/brand/favicon.png" type="image/png">
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"> -->
+    <link rel="stylesheet" href="<?= base_url('assets_backend') ?>/css/font.google.css" type="text/css">
     <!-- Icons -->
     <link rel="stylesheet" href="<?= base_url('assets_backend') ?>/vendor/nucleo/css/nucleo.css" type="text/css">
     <link rel="stylesheet" href="<?= base_url('assets_backend') ?>/vendor/%40fortawesome/fontawesome-free/css/all.min.css" type="text/css">
