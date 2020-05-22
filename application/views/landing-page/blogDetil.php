a23cdea007f61771b3dc8e0e3f3a9dc7c37f125a
bening14 <agus_salim_83@yahoo.co.id>
2020-05-22 10:10:58 +0700

    update pages detils

--- application/views/landing-page/blogDetil.php
+++ application/views/landing-page/blogDetil.php
@@ -1,77 +1,5 @@
-<!DOCTYPE html>
-<html dir="ltr" lang="en">
-
-<head>
-
-  <!-- Meta Tags -->
-  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-  <meta name="description" content="Digital Marketing Agencies, SEO companies & Social Media specialists HTML5 Template" />
-  <meta name="keywords" content="marketing,seo,ppc,mapping,linkbuilding,analytics,ads" />
-  <meta name="author" content="ThemeMascot" />
-
-  <!-- Page Title -->
-  <title>Study School College & University Template</title>
-
-  <!-- Favicon and Touch Icons -->
-  <link href="<?= base_url('assets/') ?>images/favicon.png" rel="shortcut icon" type="image/png">
-  <link href="<?= base_url('assets/') ?>images/apple-touch-icon.png" rel="icon">
-  <link href="<?= base_url('assets/') ?>images/apple-touch-icon-72x72.png" rel="icon" sizes="72x72">
-  <link href="<?= base_url('assets/') ?>images/apple-touch-icon-114x114.png" rel="icon" sizes="114x114">
-  <link href="<?= base_url('assets/') ?>images/apple-touch-icon-144x144.png" rel="icon" sizes="144x144">
-
-  <!-- Stylesheet -->
-  <link href="<?= base_url('assets/') ?>css/bootstrap.min.css" rel="stylesheet" type="text/css">
-  <link href="<?= base_url('assets/') ?>css/jquery-ui.min.css" rel="stylesheet" type="text/css">
-  <link href="<?= base_url('assets/') ?>css/animate.css" rel="stylesheet" type="text/css">
-  <link href="<?= base_url('assets/') ?>css/css-plugin-collections.css" rel="stylesheet" />
-  <!-- CSS | menuzord megamenu skins -->
-  <link href="<?= base_url('assets/') ?>css/menuzord-megamenu.css" rel="stylesheet" />
-  <link id="menuzord-menu-skins" href="<?= base_url('assets/') ?>css/menuzord-skins/menuzord-boxed.css" rel="stylesheet" />
-  <!-- CSS | Main style file -->
-  <link href="<?= base_url('assets/') ?>css/style-main.css" rel="stylesheet" type="text/css">
-  <!-- CSS | Preloader Styles -->
-  <link href="<?= base_url('assets/') ?>css/preloader.css" rel="stylesheet" type="text/css">
-  <!-- CSS | Custom Margin Padding Collection -->
-  <link href="<?= base_url('assets/') ?>css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
-  <!-- CSS | Responsive media queries -->
-  <link href="<?= base_url('assets/') ?>css/responsive.css" rel="stylesheet" type="text/css">
-  <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
-  <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
-
-  <!-- CSS | Theme Color -->
-  <link href="<?= base_url('assets/') ?>css/colors/theme-skin-color-set1.css" rel="stylesheet" type="text/css">
-  <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
-
-  <!-- external javascripts -->
-  <script src="<?= base_url('assets/') ?>js/jquery-2.2.4.min.js"></script>
-  <script src="<?= base_url('assets/') ?>js/jquery-ui.min.js"></script>
-  <script src="<?= base_url('assets/') ?>js/bootstrap.min.js"></script>
-  <!-- JS | jquery plugin collection for this theme -->
-  <script src="<?= base_url('assets/') ?>js/jquery-plugin-collection.js"></script>
-
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-</head>
-
 <body class="">
   <div id="wrapper">
-    <!-- preloader -->
-    <!-- <div id="preloader">
-      <div id="spinner">
-        <div class="preloader-dot-loading">
-          <div class="dfg"><i></i><i></i><i></i><i></i></div>
-        </div>
-      </div>
-      <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
-    </div> -->
-
-
-
 
     <!-- Start main-content -->
     <div class="main-content">
@@ -151,9 +79,6 @@
   </div>
   <!-- end wrapper -->
 
-  <!-- Footer Scripts -->
-  <!-- JS | Custom script for all pages -->
-  <!-- <script src="<?= base_url('assets/') ?>js/custom.js"></script> -->
 
 </body>
 
